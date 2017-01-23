21ecdff60e625b6cb62c4e45a0cf1e49eadb2915
Yong Wen Chua <me@yongwen.xyz>
2017-01-23 09:42:13 +0800

    Now you can clone

--- src/main.rs
+++ src/main.rs
@@ -84,6 +84,7 @@ fn read_config(path: &str) -> Result<Config, Box<Error>> {
 
 fn clone_or_open(repo_details: &RepositoryConfiguration) -> Result<Repository, git2::Error> {
     let repo = open(&repo_details.checkout_path).or_else(|err| if err.code() == git2::ErrorCode::NotFound {
+        info!("Repository not found at {} -- cloning", repo_details.checkout_path);
         clone(repo_details)
     } else {
         Err(err)
@@ -92,10 +93,12 @@ fn clone_or_open(repo_details: &RepositoryConfiguration) -> Result<Repository, g
 }
 
 fn open(checkout_path: &str) -> Result<Repository, git2::Error> {
+    info!("Opening repository at {}", checkout_path);
     Repository::open(checkout_path)
 }
 
 fn clone(repo_details: &RepositoryConfiguration) -> Result<Repository, git2::Error> {
+    debug!("Making remote authentication callbacks");
     let mut remote_callbacks = git2::RemoteCallbacks::new();
     remote_callbacks.credentials(|_uri, username, cred_type| {
             let username = username.or_else(|| match repo_details.username {
@@ -104,7 +107,8 @@ fn clone(repo_details: &RepositoryConfiguration) -> Result<Repository, git2::Err
             });
             if cred_type.intersects(git2::USERNAME) && username.is_some() {
                 git2::Cred::username(username.as_ref().unwrap())
-            } else if cred_type.intersects(git2::USER_PASS_PLAINTEXT) && username.is_some() && password.is_some() {
+            } else if cred_type.intersects(git2::USER_PASS_PLAINTEXT) && username.is_some() &&
+                      repo_details.password.is_some() {
                 git2::Cred::userpass_plaintext(username.as_ref().unwrap(),
                                                repo_details.password.as_ref().unwrap())
             } else if cred_type.intersects(git2::SSH_KEY) && repo_details.key.is_some() && username.is_some() {
@@ -116,9 +120,22 @@ fn clone(repo_details: &RepositoryConfiguration) -> Result<Repository, git2::Err
                 Err(git2::Error::from_str("Missing credentials to authenticate with remote Git repository"))
             }
         })
-        .sideband_progress(|text: &[u8]| {
-            info!("{:?}", text);
+        .transfer_progress(|progress| {
+            // TODO: Maybe throttle this, or update UI
+            info!("Received Objects: {}/{}\nIndexed Objects: {}\nIndexed Deltas: {}/{}\nBytes Received: {}",
+                progress.received_objects(), progress.total_objects(),
+                progress.indexed_objects(),
+                progress.indexed_deltas(), progress.total_deltas(),
+                progress.received_bytes());
             true
         });
-    Err(git2::Error::from_str("Oh no"))
+
+    let mut fetch_optoons =  git2::FetchOptions::new();
+    fetch_optoons.remote_callbacks(remote_callbacks);
+
+    let mut repo_builder = git2::build::RepoBuilder::new();
+    repo_builder.fetch_options(fetch_optoons);
+
+    info!("Cloning repository from {} into {}", repo_details.uri, repo_details.checkout_path);
+    repo_builder.clone(&repo_details.uri, &std::path::Path::new(&repo_details.checkout_path))
 }

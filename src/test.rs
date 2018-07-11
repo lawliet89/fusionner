84a17203b70cd15bf4354f5cd9d79064c54ba403
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-07-11 16:11:27 +0800

    Cargo fmt

--- src/test.rs
+++ src/test.rs
@@ -6,39 +6,63 @@ use url::Url;
 use super::git2;
 
 macro_rules! not_err {
-    ($e:expr) => (match $e {
-        Ok(e) => e,
-        Err(e) => panic!("{} failed with {}", stringify!($e), e),
-    })
+    ($e:expr) => {
+        match $e {
+            Ok(e) => e,
+            Err(e) => panic!("{} failed with {}", stringify!($e), e),
+        }
+    };
 }
 
 macro_rules! is_err {
-    ($e:expr) => (match $e {
-        Ok(e) => panic!("{} did not return with an error, but with {:?}", stringify!($e), e),
-        Err(e) => e,
-    })
+    ($e:expr) => {
+        match $e {
+            Ok(e) => panic!(
+                "{} did not return with an error, but with {:?}",
+                stringify!($e),
+                e
+            ),
+            Err(e) => e,
+        }
+    };
 }
 
 macro_rules! is_none {
-    ($e:expr) => ({
+    ($e:expr) => {{
         let e = $e;
-        assert!(e.is_none(), "{} was expected to be None, but was {:?}", stringify!($e), e);
-    })
+        assert!(
+            e.is_none(),
+            "{} was expected to be None, but was {:?}",
+            stringify!($e),
+            e
+        );
+    }};
 }
 
 macro_rules! not_none {
-    ($e:expr) => (match $e {
-        Some(e) => e,
-        None => panic!("{} failed with None", stringify!($e)),
-    })
+    ($e:expr) => {
+        match $e {
+            Some(e) => e,
+            None => panic!("{} failed with None", stringify!($e)),
+        }
+    };
 }
 
 macro_rules! assert_matches {
-    ($e: expr, $p: pat) => (assert_matches!($e, $p, ()));
-    ($e: expr, $p: pat, $f: expr) => (match $e {
-        $p => $f,
-        e @ _ => panic!("{}: Expected pattern {} \ndoes not match {:?}", stringify!($e), stringify!($p), e)
-    })
+    ($e:expr, $p:pat) => {
+        assert_matches!($e, $p, ())
+    };
+    ($e:expr, $p:pat, $f:expr) => {
+        match $e {
+            $p => $f,
+            e @ _ => panic!(
+                "{}: Expected pattern {} \ndoes not match {:?}",
+                stringify!($e),
+                stringify!($p),
+                e
+            ),
+        }
+    };
 }
 
 #[allow(dead_code)]
@@ -54,8 +78,7 @@ pub fn raw_repo_init() -> (TempDir, git2::Repository) {
 
         let tree = repo.find_tree(id).unwrap();
         let sig = repo.signature().unwrap();
-        repo.commit(Some("HEAD"), &sig, &sig, "initial", &tree, &[])
-            .unwrap();
+        repo.commit(Some("HEAD"), &sig, &sig, "initial", &tree, &[]).unwrap();
 
         repo.remote("origin", &path2url(&td.path())).unwrap();
     }
